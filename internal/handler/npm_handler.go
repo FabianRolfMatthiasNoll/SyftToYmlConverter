013d36093ff362b56d859058b30efa4c2dea3492
fanoll <Fabian.Noll@karlstorz.com>
2023-02-27 15:33:52 +0100

    added functionality to handle npm packages

--- internal/handler/npm_handler.go
+++ internal/handler/npm_handler.go
@@ -2,11 +2,15 @@ package handler
 
 import (
 	"syfttoymlconverter/internal"
+	"syfttoymlconverter/internal/api_interfaces"
 	"syfttoymlconverter/internal/model"
 )
 
 type Npm struct{}
 
 func (Npm) FetchMetadata(syft *internal.Syft) (model.BuildInfo, error) {
-	return model.BuildInfo{}, nil
+	var npm api_interfaces.NPM
+	models, _ := npm.ParseEmbeddedModules(syft)
+	npm.SetRepoInfo(syft, &models)
+	return models, nil
 }
