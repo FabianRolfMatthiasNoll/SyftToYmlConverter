a79291d232817e97389e35cf85448512aa20d642
Fabian <Fabian.Noll@googlemail.com>
2023-02-28 10:25:22 +0100

    removed unused functions

--- cmd/api_manager.go
+++ cmd/api_manager.go
@@ -1,22 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
-	"strings"
 	"syfttoymlconverter/internal"
-	"syfttoymlconverter/internal/api_interfaces"
 	"syfttoymlconverter/internal/model"
 
 	"github.com/goccy/go-yaml"
 )
-
-type API_Interface interface {
-	GetData(string) ([]byte, error)
-	ConverseDataToStruct([]byte, string) (model.Library, error)
-	CreateAPILink(string) string
-}
 
 type Lang_Interface interface {
 	FetchMetadata(syft *internal.Syft) (model.BuildInfo, error)
@@ -38,7 +29,7 @@ func (m *Manager) Run(syft *internal.Syft) error {
 
 	models, _ = m.Lang.FetchMetadata(syft)
 
-	libraries = api_interfaces.ModelToLibrary(&models)
+	libraries = model.ModelToLibrary(&models)
 
 	yamlData, yamlErr := yaml.Marshal(&libraries)
 	if yamlErr != nil {
@@ -52,41 +43,4 @@ func (m *Manager) Run(syft *internal.Syft) error {
 		log.Print(yamlErr)
 	}
 	return nil
-}
-
-func GetPackageName(purl string) string {
-	if strings.Contains(purl, "dotnet") {
-		//purl = strings.Replace(purl, "pkg:dotnet", "https://www.nuget.org/packages", -1)
-		pos := strings.Index(purl, "@")
-		pos2 := strings.LastIndex(purl, "/")
-		purl = purl[pos2+1 : pos]
-	}
-	return purl
-}
-
-func DisplayLibraries(libraries []model.Library) {
-	for _, l := range libraries {
-		fmt.Println("============================================================")
-		fmt.Println("Source: ", l.Source)
-		fmt.Println("Submodule: ", l.Submodule)
-		fmt.Println("Release: ", l.Release)
-		fmt.Println("Manufacturer: ", l.LibraryData.Manufacturer)
-		fmt.Println("Summary: ", l.LibraryData.Summary)
-		fmt.Println("Version: ", l.LibraryData.Version)
-		fmt.Println("License: ", l.LibraryData.License)
-		fmt.Println("Function: ", l.LibraryData.Function)
-		fmt.Println("Incorporated: ", l.LibraryData.Incorporated)
-		fmt.Println("LevelOfConcern: ", l.LibraryData.LevelOfConcern)
-
-	}
-
-	// for _, d := range models.Modules {
-	// 	fmt.Println("===========================================")
-	// 	fmt.Println(d.Version)
-	// 	fmt.Println(d.Path)
-	// 	fmt.Println(d.SubPath)
-	// 	fmt.Println(d.Info.SPDX)
-	// 	fmt.Println(d.Info.Description)
-	// 	fmt.Println(d.Info.Release)
-	// }
 }
