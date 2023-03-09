115b48f7af07ca71c117766b6ca7c68e3a594453
Fabian <Fabian.Noll@googlemail.com>
2023-03-09 10:20:49 +0100

    added functionality

---
+++ internal/tool_converter.go
@@ -0,0 +1,46 @@
+package internal
+
+import (
+	"strings"
+	"syfttoymlconverter/internal/model"
+)
+
+func ToolToDependencies(syft *Syft) *model.SBOM {
+	result := &model.SBOM{}
+
+	for _, d := range syft.Artifacts {
+		//Generating Language out of the start of the purl
+		language := strings.Split(d.Purl, "/")[0][4:]
+
+		if !contains(result.Languages, language) {
+			result.Languages = append(result.Languages, language)
+		}
+		// TopLevel is really only important for docker and angular still have to think of the way
+		// var toplevel = false
+		// for _, l := range syft.ArtifactRelationships {
+		// 	if l.Parent == d.ID {
+		// 		toplevel = true
+		// 	}
+		// }
+
+		result.Dependencies = append(result.Dependencies, model.Dependencies{
+			ImportName: d.Name,
+			Language:   language,
+			Version:    d.Version,
+			Licenses:   d.Licenses,
+			//Not sure if we should keep purl for tool independency
+			Purl: d.Purl,
+			ID:   d.ID,
+		})
+	}
+	return result
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
