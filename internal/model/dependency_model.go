7b24b6ffa1c67eb30c93f43f83d68a0f8d201c0e
Fabian <Fabian.Noll@googlemail.com>
2023-03-09 10:26:03 +0100

    restructured struct

--- internal/model/dependency_model.go
+++ internal/model/dependency_model.go
@@ -1,16 +1,19 @@
 package model
 
+type SBOM struct {
+	Name         string   `json:"name"`
+	Languages    []string `json:"languages"`
+	Dependencies []Dependencies
+}
+
 type Dependencies struct {
-	Name         string         `json:"name"`
-	Languages    map[string]int `json:"languages"`
-	Dependencies []struct {
-		Name     string   `json:"name"`
-		Language string   `json:"language"`
-		Version  string   `json:"version"`
-		Licenses []string `json:"licenses"`
-		Purl     string   `json:"purl"`
-		Url      string   `json:"url"`
-		ID       string   `json:"id"`
-		TopLevel bool     `json:"toplevel"`
-	}
+	ImportName string   `json:"importname"`
+	Language   string   `json:"language"`
+	Version    string   `json:"version"`
+	Licenses   []string `json:"licenses"`
+	//Not sure if we should keep purl for tool independency
+	Purl string `json:"purl"`
+	//Url      string `json:"url"`
+	ID string `json:"id"`
+	//TopLevel bool   `json:"toplevel"`
 }
