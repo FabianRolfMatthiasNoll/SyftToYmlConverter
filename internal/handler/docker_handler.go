b9ada137d91219ffa2f45b68c1c27a11f6b353b3
Fabian <Fabian.Noll@googlemail.com>
2023-03-09 10:25:46 +0100

    implemented Interface

--- internal/handler/docker_handler.go
+++ internal/handler/docker_handler.go
@@ -1,1 +1,12 @@
 package handler
+
+import (
+	"syfttoymlconverter/internal"
+	"syfttoymlconverter/internal/model"
+)
+
+type Docker struct{}
+
+func (Docker) FetchMetadata(syft *internal.Syft) (model.BuildInfo, error) {
+	return model.BuildInfo{}, nil
+}
